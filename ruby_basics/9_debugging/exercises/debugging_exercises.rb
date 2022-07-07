b9712f7e2ca91e82b49a9c8f98e60e0b3b9beb42
Dainelli <daniel.sf.freire@gmail.com>
2022-07-07 21:53:21 +0100

    debugging

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -21,10 +21,10 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
+  greatest_value = nested_array.flatten.max
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < 'nil'.to_i
         greatest_value = current_value
       end
     end
@@ -36,10 +36,7 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
+  string.downcase.chars.uniq == string.downcase.chars
 end
 
 # Can you guess what's next?
@@ -51,6 +48,6 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
 end
